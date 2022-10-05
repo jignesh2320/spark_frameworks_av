0333550e59da4ec0d0b952c3b3497ed2ac842379
Gareth Fenn <gareth.fenn@broadcom.com>
2022-10-05 11:45:45 -0700

    Resume HW after forced pause during flush
    Test: see bug, compiles
    Bug: 226349461
    Change-Id: I48b41a79a04c42d3ca05bb66f1bc43122ed1568a

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -6491,6 +6491,7 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::DirectOutputThread::prep
             (doHwPause || (mFlushPending && !mHwPaused && (count != 0)))) {
         status_t result = mOutput->stream->pause();
         ALOGE_IF(result != OK, "Error when pausing output stream: %d", result);
+        doHwResume = !doHwPause;  // resume if pause is due to flush.
     }
     if (mFlushPending) {
         flushHw_l();
@@ -7061,6 +7062,7 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::OffloadThread::prepareTr
     if (!mStandby && (doHwPause || (mFlushPending && !mHwPaused && (count != 0)))) {
         status_t result = mOutput->stream->pause();
         ALOGE_IF(result != OK, "Error when pausing output stream: %d", result);
+        doHwResume = !doHwPause;  // resume if pause is due to flush.
     }
     if (mFlushPending) {
         flushHw_l();
